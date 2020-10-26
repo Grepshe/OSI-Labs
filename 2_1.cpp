f2c2d775f63954eb080ada745becb46df80d60b5
Greg <grepshe@gmail.com>
2020-10-27 01:28:42 +0300

    Initial commit

---
+++ 2_1.cpp
@@ -0,0 +1,17 @@
+#include <filesystem>
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  setlocale(LC_ALL, "Russian");
+  system("chcp 1251");
+  string dir;
+  cin >> dir;
+  for (const filesystem::directory_entry& i : filesystem::directory_iterator(dir))
+      cout << i.path().string() << endl;
+  return 0;
+}
